b6c0598499b79ffc1c71d593318214cec0d81ab4
Maxim <KrMax-steam@yandex.ru>
2018-08-21 17:10:20 +0300

    Add UsartQueue,I2cQueue* in PollingRegistersModule()
    Create gpio_queue, _usart_queue, _i2c_queue and Message

--- emb/Module/polling_register.cpp
+++ emb/Module/polling_register.cpp
@@ -1,15 +1,21 @@
 #include "Module/polling_registers.h"
 
-PollingRegistersModule::PollingRegistersModule(GpioQueue *gpio_queue) :
-    _gpio_queue(gpio_queue)
+PollingRegistersModule::PollingRegistersModule(GpioQueue *gpio_queue,
+                                               UsartQueue* usart_queue,
+                                               I2cQueue* i2c_queue) :
+    _gpio_queue(gpio_queue), _usart_queue(usart_queue), _i2c_queue(i2c_queue)
 {
 
 }
 
 void PollingRegistersModule::run()
 {
-    GpioModule::Message msg;
+    GpioModule::Message gpio_msg;
+    UsartModule::Message usart_msg;
+    I2cModule::Message i2c_msg;
 
-    _gpio_queue->push(msg);
+    _gpio_queue->push(gpio_msg);
+    _usart_queue->push(usart_msg);
+    _i2c_queue->push(i2c_msg);
 }
 
--- emb/Module/polling_registers.h
+++ emb/Module/polling_registers.h
@@ -2,16 +2,23 @@
 
 #include <etl/queue.h>
 #include "gpio_module.h"
+#include "usart_module.h"
+#include "i2c_module.h"
 
 class PollingRegistersModule
 {
     using GpioQueue = GpioModule::Queue;
+    using UsartQueue = UsartModule::Queue;
+    using I2cQueue = I2cModule::Queue;
 public:
 
-    PollingRegistersModule(GpioQueue *gpio_queue);
+    PollingRegistersModule(GpioQueue *gpio_queue, UsartQueue* usart_queue,
+                           I2cQueue* i2c_queue);
     void run();
 
 private:
     GpioQueue* _gpio_queue;
+    UsartQueue* _usart_queue;
+    I2cQueue* _i2c_queue;
 };
 
