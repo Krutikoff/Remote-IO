c53a6e52a829a0b7d729ec8d1401fecc1cdf9f62
Maxim <KrMax-steam@yandex.ru>
2018-08-21 15:42:34 +0300

    Add GpioModule class

---
+++ emb/Module/gpio_module.cpp
@@ -0,0 +1,5 @@
+#include "Module/gpio_module.h"
+
+void GpioModule::run() {
+
+}
---
+++ emb/Module/gpio_module.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <cstdint>
+#include <etl/queue.h>
+
+class GpioModule
+{
+public:
+    enum Action
+    {
+        WRITE, READ, SET,
+    };
+
+    struct Message
+    {
+        Action action;
+        uint32_t address;
+        uint32_t value;
+    };
+
+    using Queue = etl::queue<Message, 7>;
+
+    void run();
+
+private:
+    Queue _queue;
+};
