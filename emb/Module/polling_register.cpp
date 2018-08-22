da240883e24ee8382243c471de8623b2eaa2c5eb
Maxim <KrMax-steam@yandex.ru>
2018-08-22 19:20:15 +0300

    Add polling gpio and latch registers

--- emb/Module/polling_register.cpp
+++ emb/Module/polling_register.cpp
@@ -10,36 +10,61 @@ PollingRegistersModule::PollingRegistersModule(GpioQueue *gpio_queue,
 
 void PollingRegistersModule::run()
 {
-//    GpioModule::Message gpio_msg;
-//    UsartModule::Message usart_msg;
-//    I2cModule::Message i2c_msg;
-
-    _gpio_queue->push(_gpio_msg);
-    _usart_queue->push(_usart_msg);
-    _i2c_queue->push(i2c_msg);
+    _polling_gpio_registers();
+    _polling_latch_registers();
 }
 
 void PollingRegistersModule::_polling_gpio_registers()
 {
     Modbus& modbus = Modbus::instance();
-    auto gpio_setup_registers = modbus.get_iterator<GpioModule::GpioMode>(Coil::GPIO_SET_BIT0);
+    auto gpio_setup_registers = modbus.get_iterator<GpioModule::GpioMode>(
+        Coil::GPIO_SET_BIT0);
 
-    for (uint32_t i = 0U; i < cached_gpio_mode.size(); ++i) {
+    for (uint32_t i = 0U; i < _cached_gpio_mode.size(); ++i) {
         auto current_pin_mode = gpio_setup_registers[i];
 
-
-        if (cached_gpio_mode[i] != current_pin_mode) {
+        if (_cached_gpio_mode[i] != current_pin_mode) {
             _gpio_msg.address = static_cast<GpioModule::Address>(i);
-            _gpio_msg.action = (current_pin_mode == GpioModule::GpioMode::WRITE) ?
-                GpioModule::Action::SETUP_WRITE :
-                GpioModule::Action::SETUP_READ;
-
+            _gpio_msg.action =
+                (current_pin_mode == GpioModule::GpioMode::WRITE) ?
+                    GpioModule::Action::SETUP_WRITE : GpioModule::Action::SETUP_READ;
             _gpio_queue->push(_gpio_msg);
         }
 
-        cached_gpio_mode[i] = current_pin_mode;
+        _cached_gpio_mode[i] = current_pin_mode;
     }
 
+    //Write 0 or 1 to GPIO_WRITE registers Modbus
+    auto gpio_write_registers = modbus.get_iterator<uint8_t>(Coil::GPIO_WRITE_BIT0);
+
+    for (uint32_t i = 0U; i < _cached_gpio_write.size(); ++i) {
+        auto current_gpio_write = gpio_write_registers[i];
+
+        if (_cached_gpio_write[i] != current_gpio_write) {
+            _gpio_msg.action = GpioModule::Action::SET_PIN;
+            _gpio_msg.address = static_cast<GpioModule::Address>(i);
+            _gpio_msg.value = (current_gpio_write);
+            _gpio_queue->push(_gpio_msg);
+        }
+        _cached_gpio_write[i] = current_gpio_write;
+    }
+
+    //Read from GPIO_READ registers Modbus
 
 }
 
+void PollingRegistersModule::_polling_latch_registers()
+{
+    Modbus& modbus = Modbus::instance();
+    //Latch Clear
+    auto latch_status = modbus.get_iterator<GpioModule::LatchStatus>(Coil::LATCH_CLEAR);
+    if (*latch_status == GpioModule::LatchStatus::SET) {
+        _gpio_msg.action = GpioModule::Action::LATCH_CLEAR;
+        _gpio_msg.value = static_cast<uint32_t>(*latch_status);
+        *latch_status = GpioModule::LatchStatus::RESET;
+    }
+
+
+
+
+}
--- emb/Module/polling_registers.h
+++ emb/Module/polling_registers.h
@@ -20,18 +20,22 @@ public:
 
 private:
 
-
-
     GpioQueue* _gpio_queue;
     UsartQueue* _usart_queue;
     I2cQueue* _i2c_queue;
 
     GpioModule::Message _gpio_msg;
     UsartModule::Message _usart_msg;
-    I2cModule::Message i2c_msg;
-    etl::array<GpioModule::GpioMode, 8> cached_gpio_mode = { GpioModule::GpioMode::READ };
+    I2cModule::Message _i2c_msg;
+
+    etl::array<GpioModule::GpioMode, 8> _cached_gpio_mode = { GpioModule::GpioMode::READ };
+    etl::array<uint8_t, 8> _cached_gpio_write = { 0 };
+    etl::array<uint8_t, 8> _cached_gpio_read = { 0 };
+    etl::array<GpioModule::LatchStatus, 8> _cached_latch_status = { GpioModule::LatchStatus::LOW };
 
     void _polling_gpio_registers();
+    void _polling_latch_registers();
+
     void _polling_discrete_registers();
     void _polling_input_registers();
     void _polling_holding_registers();
