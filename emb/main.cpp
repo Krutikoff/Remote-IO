f983bd72e16e7eb8ec29818c2c740c3a8c1529ff
Maxim <KrMax-steam@yandex.ru>
2018-08-29 16:34:23 +0300

    Add usart_module.h and flash_module.h, change call usart_module and flash_module

--- emb/main.cpp
+++ emb/main.cpp
@@ -1,28 +1,31 @@
 #include <Modbus/modbus.h>
 
-#include <iterator>
 #include <algorithm>
+#include <iterator>
 #include <queue>
 
 #include <ConfigModule/config_module.h>
 #include <Module/gpio_module.h>
-#include <Module/usart_module.h>
 #include <Module/i2c_module.h>
+#include <Module/usart_module.h>
+
+#include <Module/flash_module.h>
 
 int main()
 {
     Module module;
     GpioModule& gpio_module = GpioModule::instance();
-    UsartModule usart_module;
+    UsartModule& usart_module = UsartModule::instance();
     I2cModule i2c_module;
+    FlashModule& flash_module = FlashModule::instance();
 
     module.rcc_clock_config();
-    //module.remap(); REMAP FUNCRION
+    // module.remap(); REMAP FUNCRION
     module.led_setup();
-    //nvic_setup();
-    //gpio_clear(GPIOA, GPIO0);
-    //tim2_config();
-    //usart_setup();
+    // nvic_setup();
+    // gpio_clear(GPIOA, GPIO0);
+    // tim2_config();
+    // usart_setup();
 
     module.mb_gpio_config();
 
@@ -36,8 +39,9 @@ int main()
         gpio_module.run();
         usart_module.run();
         i2c_module.run();
+        flash_module.run();
 
-        modbus.mb_baudrate_set();
+        //        modbus.mb_baudrate_set();
 
         //__asm__("nop");
     }
