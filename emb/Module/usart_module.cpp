c1b386e55ca411907831071c41b37e87cc9b25b4
Maxim <KrMax-steam@yandex.ru>
2018-08-29 16:29:13 +0300

    Add _baudrate_set() and get_baudrate() methods to UsartModule class

--- emb/Module/usart_module.cpp
+++ emb/Module/usart_module.cpp
@@ -2,5 +2,25 @@
 
 void UsartModule::run()
 {
-    if (!_queue.empty()) _queue.clear();
+    _baudrate_set();
+    //if (!_queue.empty()) _queue.clear();
+}
+
+
+void UsartModule::_baudrate_set() {
+
+    Modbus& modbus = Modbus::instance();
+
+    auto baudrate_registers =
+       modbus.get_iterator<uint32_t>(Holding::UART_BAUDRATE_0);
+
+    if (_baudrate != *baudrate_registers) {
+
+        nvic_disable_irq(MODBUS_IRQ);
+        usart_disable(MODBUS_UART);
+        _baudrate = *baudrate_registers;
+        usart_set_baudrate(MODBUS_UART, _baudrate);
+        nvic_enable_irq(MODBUS_IRQ);
+        usart_enable(MODBUS_UART);
+    }
 }
