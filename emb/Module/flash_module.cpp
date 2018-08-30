a21e83c4d6a75e6dd6a7891f2edfe7f50f8c2019
Maxim <KrMax-steam@yandex.ru>
2018-08-30 11:03:16 +0300

    Add a Comment in flash_module.cpp

--- emb/Module/flash_module.cpp
+++ emb/Module/flash_module.cpp
@@ -3,30 +3,11 @@
 uint32_t FlashModule::run()
 {
 
-//    static uint32_t write_data = 12345;
-//    static uint32_t read_data = 0;
-//    uint32_t flash_status = 0;
-
-//    size_t size_flash = sizeof(_flash_data);
-//    size_t size_gpio_mode = sizeof(FlashData::gpio_mode);
-//    size_t size_gpio_write = sizeof(FlashData::gpio_write);
-//    size_t size_read = sizeof(FlashData::gpio_read);
-//    size_t size_latch = sizeof(Latch);
-    //static bool flash_switch = true;
-
     _polling_flash_data();
 
-    if(flag_write){
-        _write_data_to_flash();
-
-    }
-//    if (read_data != write_data) {
-//        flash_unlock();
-//        flash_erase_page(OPERATION_ADDRESS);
-//        flash_program_word(OPERATION_ADDRESS, write_data);
-//        flash_status = flash_get_status_flags();
-//        if (flash_status != FLASH_SR_EOP) return flash_status;
-//        read_data = *_memory_ptr;
+//    if(flag_write){
+//        _write_data_to_flash();
+//
 //    }
 
     return 0;
