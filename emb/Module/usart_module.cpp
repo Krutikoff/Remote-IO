119a0ff6e186c13d06073b798d9650fdd433bd1f
Maxim <KrMax-steam@yandex.ru>
2018-09-12 19:20:25 +0300

    Add check baudrate_registers < 65535 in usart_module.cpp

--- emb/Module/usart_module.cpp
+++ emb/Module/usart_module.cpp
@@ -14,7 +14,13 @@ void UsartModule::_baudrate_set() {
     auto baudrate_registers =
        modbus.get_iterator<uint32_t>(Holding::UART_BAUDRATE_0);
 
+
+
     if (_baudrate != *baudrate_registers) {
+
+        if(*baudrate_registers < 65535){
+            *baudrate_registers = (*baudrate_registers) & 0x0000FFFF;
+        }
 
         nvic_disable_irq(MODBUS_IRQ);
         usart_disable(MODBUS_UART);
